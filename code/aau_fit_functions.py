30f873f430c0dca60ed2f5b4e2e24ade4af22692
elliottburdett <elliottburdett@gmail.com>
2026-02-27 12:12:18 -0800

    new stuff

--- code/aau_fit_functions.py
+++ code/aau_fit_functions.py
@@ -20,14 +20,16 @@ def quad_f(phi,c1,c2,c3):
     return c1 + c2*x + c3*x**2
 
 bhb_rrl = pd.read_csv("aau_bhb_rrl.csv")  #Created with select_bhb_rrl.py
-coefficients = np.polyfit(bhb_rrl['Phi1'], bhb_rrl['Distance_Modulus'], deg=2)
+coefficients = np.polyfit(bhb_rrl['phi1'], bhb_rrl['distance_modulus'], deg=2)
 distance_fit = np.poly1d(coefficients)
 
 pmdec_params = {'c1': -0.982, 'c2': -0.089, 'c3': 0.025} #Assumed from Andrew Li's S5 AAU Members
-pmdec_fit = quad_f(phi1, pmdec_params['c1'], pmdec_params['c2'], pmdec_params['c3'])
+def pmdec_fit(phi1):
+    return quad_f(phi1, pmdec_params['c1'], pmdec_params['c2'], pmdec_params['c3'])
 
 pmra_params = {'c1': -0.164, 'c2': -0.349, 'c3': -0.057} #Assumed from Andrew Li's S5 AAU Members
-pmra_fit = quad_f(phi1, pmra_params['c1'], pmra_params['c2'], pmra_params['c3'])
+def pmra_fit(phi1): 
+    return quad_f(phi1, pmra_params['c1'], pmra_params['c2'], pmra_params['c3'])
 
 s5_aau_members = Table.read('aau_members_full.fits', format='fits')
 coefficients_left = np.polyfit(s5_aau_members[s5_aau_members['phi1'] < -11.5]['phi1'], s5_aau_members[s5_aau_members['phi1'] < -11.5]['phi2'], deg=2)
